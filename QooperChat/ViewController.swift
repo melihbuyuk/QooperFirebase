12c1f169c75bb8f06611ff362ab85d4cd9067b59
Melih Buyukbayram <melihbuyuk@gmail.com>
2020-02-01 14:37:48 +0300

    Initial Commit

---
+++ QooperChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QooperChat
+//
+//  Created by Melih Buyukbayram on 1.02.2020.
+//  Copyright © 2020 Melih Buyukbayram. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
